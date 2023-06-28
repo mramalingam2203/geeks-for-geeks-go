78a53a3caf2b097ee632e3361ab3ec1578d8e6fa
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-28 14:31:15 +0530

    add code to valid ip

--- valid-ip.go
+++ valid-ip.go
@@ -1,19 +1,34 @@
 // https://practice.geeksforgeeks.org/problems/validate-an-ip-address-1587115621
 
-
 package main
 
 import (
-	"os"
 	"fmt"
+	_ "os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	ip := "255.255.0.1"
-	isValid(ip)
+	ip := "255.255.a.1"
+	fmt.Println(isValid(ip))
 }
 
+func isValid(s string) bool {
+	delimiter := "."
+	result := strings.Split(s, delimiter)
+	var n int
+	if len(result) > 4 {
+		return false
+	} else {
+		for i := 0; i < len(result); i++ {
+			n, _ = strconv.Atoi(result[i])
+			fmt.Println(n)
+			if !(n >= 0 && n <= 255) {
+				return false
+			}
+		}
+	}
+	return true
 
-func (s string) bool{
-	
 }
