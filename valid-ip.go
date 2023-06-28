7241f34181510cce0133ba7a4270ea998d4e3acf
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-28 14:19:34 +0530

    initial code for valid ip

---
+++ valid-ip.go
@@ -0,0 +1,19 @@
+// https://practice.geeksforgeeks.org/problems/validate-an-ip-address-1587115621
+
+
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	ip := "255.255.0.1"
+	isValid(ip)
+}
+
+
+func (s string) bool{
+	
+}
