25cd21987333847b80a563fce0347719fca68336
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-03 15:27:45 +0530

    Mo's algorithm initial code

---
+++ mo-algo.go
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/mos-algorithm-query-square-root-decomposition-set-1-introduction/
+
+package main
+
+func printQuerySums(a []int, q [][]int, m int) {
+
+}
+
+
+func main() {
+	a := []int {1, 1, 2, 1, 3, 4, 5, 2, 8}
+	q := [][]int {{0,4}, {1,3}, {2,4}}
+	printQuerySums(a []int, n int, q [][]int, m int)
+}
