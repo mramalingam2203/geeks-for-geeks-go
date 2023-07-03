a264e6072e724956c5bb18cccab97e49981fe702
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-03 15:47:59 +0530

    Mo algo code checked

--- mo-algo.go
+++ mo-algo.go
@@ -2,13 +2,28 @@
 
 package main
 
-func printQuerySums(a []int, q [][]int, m int) {
+import (
+	"fmt"
+)
+
+func printQuerySums(a []int, q [][]int) {
+	//len_a := len(a)
+	len_q := len(q)
+
+	for i := 0; i < len_q; i++ {
+		sum := 0
+		for j := a[q[i][0]]; j <= q[i][1]; j++ {
+			fmt.Print(a[j], " ")
+			sum += a[j]
+		}
+		fmt.Println(sum)
+	}
 
 }
 
-
 func main() {
-	a := []int {1, 1, 2, 1, 3, 4, 5, 2, 8}
-	q := [][]int {{0,4}, {1,3}, {2,4}}
-	printQuerySums(a []int, n int, q [][]int, m int)
+	array := []int{1, 1, 2, 1, 3, 4, 5, 2, 8}
+	queries := [][]int{{0, 4}, {1, 3}, {2, 4}}
+
+	printQuerySums(array, queries)
 }
