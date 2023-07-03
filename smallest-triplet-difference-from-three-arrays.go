5f4c63babb1d910d7ab0c1a6a2c551b616dc3067
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-04 00:53:37 +0530

    init code doing

---
+++ smallest-triplet-difference-from-three-arrays.go
@@ -0,0 +1,22 @@
+// https://www.geeksforgeeks.org/smallest-difference-triplet-from-three-arrays/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+	"math"
+)
+
+func main() {
+
+}
+
+func simpleMethod(arr1 []int, arr2 []int, arr3 []int) {
+	sort.Ints(arr1)
+	sort.Ints(arr2)
+	sort.Ints(arr3)
+	var i, j, k, res_min, res_max, res_mid
+	const diff int = math.MaxInt64)
+
+}
