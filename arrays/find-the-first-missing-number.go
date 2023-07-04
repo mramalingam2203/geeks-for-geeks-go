fe3ef0c5433488bbf66329745b9d5669238cbbd5
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-04 20:44:11 +0530

    add iterative binary search code

---
+++ arrays/find-the-first-missing-number.go
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/find-the-first-missing-number/
+
+func binarySearch_iter(arr []int, target int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1 // Target not found
+}
