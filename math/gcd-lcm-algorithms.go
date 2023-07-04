5ee80b57fb0d605d6ea819f53fc6b44c3369966d
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-04 10:47:04 +0530

    one algo added

--- math/gcd-lcm-algorithms.go
+++ math/gcd-lcm-algorithms.go
@@ -1,16 +1,27 @@
 // https://www.geeksforgeeks.org/mathematical-algorithms/?ref=shm
 
-package main()
+package main
 
 import "fmt"
 
 func main() {
-
+	a := []int{1, 2, 3, 4, 9, 10, 12, 15}
+	fmt.Println(gcd(23, 2))
+	fmt.Println(lcm_of_array_elements(a))
 }
 
-func gcd(a, b int){
-    if b == 0{
-        return a
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
 	}
-    return gcd(b, a % b)
+	return gcd(b, a%b)
+}
+
+func lcm_of_array_elements(arr []int) uint64 {
+	result := uint64(arr[0])
+	for i := range arr {
+		result = ((uint64(arr[i])) * result) / (uint64(gcd(arr[i], int(result))))
+	}
+
+	return result
 }
