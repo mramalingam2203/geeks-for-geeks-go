a61208f6288d74599077b38fbfd5b40f2bdc4a17
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-04 12:58:48 +0530

    coudl get only half the hour glass :(

--- geometry/print-pyramid.go
+++ geometry/print-pyramid.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	printPyramid(5)
+	printHourglassPattern(5)
 }
 
 func printPyramid(n int) {
@@ -23,6 +24,46 @@ func printPyramid(n int) {
 		for j := i; j > 0; j-- {
 			fmt.Print(" * ")
 		}
+		fmt.Println()
+	}
+
+}
+
+func printHourglassPattern(n int) {
+
+	// for loop for printing
+	// upper half
+	for i := 1; i <= n; i++ {
+
+		// printing i spaces at
+		// the beginning of each row
+		for k := 1; k < i; k++ {
+			fmt.Print(" ")
+		}
+
+		// printing i to rows value
+		// at the end of each row
+		for j := i; j <= n; j++ {
+			fmt.Print(j)
+		}
+		fmt.Println()
+	}
+
+	// for loop for printing lower half
+	for i := n - 1; i >= 1; i-- {
+
+		// printing i spaces at the
+		// beginning of each row
+		for k := 1; k < i; k++ {
+			fmt.Print(" ")
+		}
+
+		// printing i to rows value
+		// at the end of each row
+		for j := i; j <= n; j++ {
+			fmt.Print(j)
+		}
+
 		fmt.Println()
 	}
 
