d705f984da74a2e718aa0550a419d431e6747888
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-04 19:46:04 +0530

    so many index manips

--- arrays/rotateArray.go
+++ arrays/rotateArray.go
@@ -11,17 +11,23 @@ func main() {
 }
 
 func rotateArray(array []int, d int) {
+	n := len(array)
 	block_1 := make([]int, d)
+
 	block_2 := make([]int, len(array)-d)
 
-	for i := 0; i < d; i++ {
-		block_1[d-i] = array[i]
+	for i := 0; i <= d-1; i++ {
+		block_1[i] = array[d-(i+1)]
+		//fmt.Print(i, " ")
 	}
 
-	for i := 0; i < d; i++ {
-		block_1[d-i] = array[i]
+	for i := d; i < n; i++ {
+		fmt.Print(i, " ")
+		block_2[i] = array[n-(i+1)]
+		//block_2[len(array)-d-i] = array[len(array)-d-(i+1)]
 	}
 
-	fmt.Println(block_1)
+	//fmt.Println(block_1)
+	//fmt.Println(block_2)
 
 }
