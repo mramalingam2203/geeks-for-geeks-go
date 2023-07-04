f094a754732b76900f2054e954ef8abd20952234
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-04 09:00:47 +0530

    done

--- rearrangeArrays.go
+++ rearrangeArrays.go
@@ -16,11 +16,11 @@ func main() {
 
 func rearrangeArray(arr []int) {
 	var tmp int
-	for i := 0; i < len(arr); i++ {
-		if arr[i+1]%2 == 0&arr1[i+2]%2 != 0 {
-			tmp = arr[i+2]
-			arr[i+1] = arr[i+2]
-			arr[i+2] = tmp
+	for i := 1; i < len(arr)-1; i += 2 {
+		if arr[i] < arr[i+1] {
+			tmp = arr[i]
+			arr[i] = arr[i+1]
+			arr[i+1] = tmp
 		}
 	}
 	fmt.Println(arr)
