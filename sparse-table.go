8905fb4b2135e8e1b5ef908cf008c6ff6873586c
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-06 01:41:20 +0530

    range minimum query

---
+++ sparse-table.go
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/sparse-table/
+
+package main
+
+func main() {
+	arr := []int{7, 2, 3, 0, 5, 10, 3, 12, 18}
+	query := [][]int{{0, 4}, {4, 7}, {7, 8}}
+
+}
+
+func range_minimum_query(array []int, queries [][]int) {
+	for 
+
+}
