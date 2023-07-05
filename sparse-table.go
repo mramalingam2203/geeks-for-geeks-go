6b257a10422f55cd126d5565d229889732823cdf
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-06 01:49:38 +0530

    managed two loops

--- sparse-table.go
+++ sparse-table.go
@@ -2,13 +2,27 @@
 
 package main
 
+import "fmt"
+
 func main() {
 	arr := []int{7, 2, 3, 0, 5, 10, 3, 12, 18}
 	query := [][]int{{0, 4}, {4, 7}, {7, 8}}
 
+	range_minimum_query(arr, query)
+
 }
 
 func range_minimum_query(array []int, queries [][]int) {
-	for 
+	for i := range queries {
+		for j := queries[i][0]; j <= queries[i][1]; j++ {
+			
+			temp := array[j]
+			
+			if array[j+1] > max {
+			
+
+		}
+		fmt.Println()
+	}
 
 }
