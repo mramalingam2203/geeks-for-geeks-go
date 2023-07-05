ebd07c904e535a2ac831326a58cb082f988e854e
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-05 16:38:16 +0530

    sundaram seive addedd

--- math/prime-number-matters.go
+++ math/prime-number-matters.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	fmt.Println(naive(31))
 	fmt.Println(efficient(20))
+	sundaram(203)
 
 }
 
@@ -42,4 +43,34 @@ func efficient(n int) bool {
 	}
 	return true
 
+}
+
+func sundaram(n int) {
+	// Since we want primes smaller than n, we reduce n to half
+	nNew := (n - 1) / 2
+
+	// This array is used to separate numbers of the form i+j+2ij
+	// from others where  1 <= i <= j
+	marked := make([]bool, nNew+1)
+
+	// Main logic of Sundaram.  Mark all numbers of the
+	// form i + j + 2ij as true where 1 <= i <= j
+	for i := 1; i <= nNew; i++ {
+		for j := i; i+j+2*i*j <= nNew; j++ {
+			marked[i+j+2*i*j] = true
+		}
+	}
+
+	// Since 2 is a prime number
+	if n > 2 {
+		fmt.Print("2", " ")
+	}
+
+	// Print other primes. Remaining primes are of the form
+	// 2*i + 1 such that marked[i] is false.
+	for i := 1; i <= nNew; i++ {
+		if marked[i] == false {
+			fmt.Print(2*i+1, " ")
+		}
+	}
 }
