4caddb20d69880201c7b7ce78f3e733d65f2631e
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-05 10:03:28 +0530

    add init code

---
+++ arrays/print-distinct-elements.go
@@ -0,0 +1,19 @@
+// Print All Distinct Elements of a given integer array
+
+// https://www.geeksforgeeks.org/print-distinct-elements-given-integer-array/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	array := []int{1, 2, 3, 4, 4, 3, 5, 2, 2, 1, 2, 3, 4, 5}
+	distinct_elements(array)
+
+}
+
+func distinct_elements(arr []int) {
+
+}
