93d663498e818b771f25132d5d0d0e7dabb84b94
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-05 14:52:46 +0530

    bad logic

--- math/baseRepresentation.go
+++ math/baseRepresentation.go
@@ -7,14 +7,18 @@ import (
 )
 
 func main() {
+	fmt.Println(checkBase(8, 4))
 }
 
 func checkBase(m, n int) bool {
 	for i := 2; i <= 32; i++ {
 		if m < i && n == 1 {
 			return true
+		} else if m > i && n > 1 {
+			m = m / i
+			checkBase(m, n)
 		}
-
 	}
+	return false
 
 }
