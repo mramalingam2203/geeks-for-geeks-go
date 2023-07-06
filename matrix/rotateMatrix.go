802a1bb843e4a53668287d0184a6f2768159e058
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-06 10:41:03 +0530

    reverse columns

--- matrix/rotateMatrix.go
+++ matrix/rotateMatrix.go
@@ -1,23 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	matrix := [][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}}
-	transposematrix(matrix)
+	matrix = transposematrix(matrix)
+
 }
 
-func transposematrix(mat [][]int) {
-	result := make([]int, 0, len(mat)*len(mat))
+func transposematrix(mat [][]int) [][]int {
+	rows := len(mat)
+	cols := len(mat[1])
 
-	for i := 0; i < len(mat); i++ {
-		fmt.Println()
-		for j := 0; j < len(mat); j++ {
-			result = append(result, mat[i][j])
+	// Create a new matrix with dimensions swapped
+	transposed := make([][]int, cols)
+	for i := 0; i < cols; i++ {
+		transposed[i] = make([]int, rows)
+	}
+
+	// Fill the transposed matrix with values from the original matrix
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			transposed[j][i] = mat[i][j]
 		}
 	}
 
-	fmt.Println(result)
+	return transposed
+
+}
+
+func reverseColumns(mat [][]int) [][]int {
+
 }
