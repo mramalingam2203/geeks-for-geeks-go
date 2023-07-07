b2e782db3738b1f1f97a50b818c49ee98c365537
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-07 08:22:57 +0530

    mostly refactored

--- queue/simple-exercises.go
+++ queue/simple-exercises.go
@@ -1,73 +1,73 @@
-
 // Write a golnag code m to implement a queue using an array. Programs should contain functions for inserting elements into the queue, displaying queue elements, and checking whether the queue is empty or not
 // queue using an array
 
 package main
 
-import(
+import (
 	"fmt"
 )
 
+const MAX_SIZE int = 100
 
-MAX_SIZE const int = 100
+var queue = make([]int, MAX_SIZE)
 
-queue = [MAX_SIZE]int
-front := -1
-rear := -1
+//var queue [100]int
 
-func enQueue(element int){
-	if rear == MAX_SIZE -1 {
+var front, rear int = -1, -1
+
+func enQueue(element int) {
+	if rear == MAX_SIZE-1 {
 		fmt.Println("queue is full. cannot enqueue")
-	}else{
+	} else {
 		if front == -1 {
 			front = 0
 		}
-		rear ++
+		rear++
 		queue[rear] = element
 		fmt.Println("enqueued successfully")
 	}
 }
 
-func enQueue( ){
-	if front == -1 || front > rear{
-		fmt.Println("queue is empty. cannot enqueue"
-	}else{
+func deQueue() {
+	if front == -1 || front > rear {
+		fmt.Println("queue is empty. cannot enqueue")
+	} else {
 		fmt.Println("%d enqueued", queue[front])
 		front++
 	}
 }
 
-func isEmpty() {
-    if (front == -1 || front > rear) {
-        return 1;
-    } else {
-        return 0;
-    }
+func isEmpty() bool {
+	if front == -1 || front > rear {
+		return true
+	} else {
+		return false
+	}
 }
 
-func  display() {
-    if (isEmpty()) {
-        fmt.Println("Queue is empty.\n");
-    } else {
-        printf("Queue elements: ")
-        for (int i = front; i <= rear; i++) {
-            fmt.Println("%d ", queue[i])
-        }
-        fmt.Println( )
-    }
+func display() {
+	if isEmpty() == true {
+		fmt.Println("Queue is empty.\n")
+	} else {
+		printf("Queue elements: ")
+		for i := front; i <= rear; i++ {
+			fmt.Println("%d ", queue[i])
+		}
+		fmt.Println()
+	}
 }
 
 func main() {
-    enqueue(10)
-    enqueue(20)
-    enqueue(30)
-    display()
-    
-    dequeue()
-    display()
-    
-    dequeue()
-    dequeue()
-    display()
+	enQueue(10)
+	enQueue(20)
+	enQueue(30)
+	display()
+
+	dequeue()
+	display()
+
+	dequeue()
+	dequeue()
+	display()
 
 }
