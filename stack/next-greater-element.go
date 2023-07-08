975530be6cd531c6fcc327963cfa05b0bdb3aa86
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-08 11:00:01 +0530

    not working :(

--- stack/next-greater-element.go
+++ stack/next-greater-element.go
@@ -2,21 +2,31 @@
 
 package main
 
+import "fmt"
 
 func main() {
 	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	printNGE(array []int)
+	printNGE(array)
 
 }
 
 func printNGE(arr []int) {
 	stack := new(Stack)
 	stack.push(arr[0])
-	for  i := 1; i < n; i++ {
+	for i := 1; i < len(arr); i++ {
 		if stack.isEmpty() {
-            stack.push(arr[i])
-            continue
-        }
+			stack.push(arr[i])
+			continue
+		}
+		fmt.Println(stack.top.data)
 
+		for {
+			if stack.top.data-arr[i] == true {
+				fmt.Println(stack.top.data, " --> ", arr[i])
+			}
+		}
+
+		stack.push(arr[i])
 
 	}
+}
