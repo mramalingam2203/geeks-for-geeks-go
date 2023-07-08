ee177080821eff6eedae0590ac0b59ff0e33d048
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-08 11:24:18 +0530

    freq code runnign

--- stack/next-greater-freq.go
+++ stack/next-greater-freq.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
 // Stack definition start
@@ -76,32 +77,38 @@ func (stack *Stack) Drain() {
 
 func maxArray(array []int) int {
 	max := 0
-	for i:= 0; i < len(array) ; i++{
-		if array[i] > max{
+	for i := 0; i < len(array); i++ {
+		if array[i] > max {
 			max = array[i]
 		}
 	}
 	return max
 }
 
-
 // next greater frequency function
-func NFG(array int[]){
+func NFG(array []int) {
 
 	max := maxArray(array)
 	freq := make([]int, max+1)
-	
-    // Calculating frequency of each element
-    for (int i = 0; i < len; i++){
-        freq[array[i]]++
-    }
+
+	// Calculating frequency of each element
+	for i := 0; i < len(array); i++ {
+		freq[array[i]]++
+	}
 
 	fmt.Println(freq)
-}
 
+	stack := NewStack(100)
+	stack.Push(0)
+
+	for i := 1; i < len(array); i++ {
+
+	}
+
+}
 
 func main() {
+	array := []int{1, 2, 3, 4, 5, 6, 7}
+	NFG(array) //
 
 }
-
-
