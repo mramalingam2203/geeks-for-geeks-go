a236990b89ebc5065f315744257bd48aaaeaed7a
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-08 08:50:07 +0530

    naive approach

--- stack/stock-span-problem.go
+++ stack/stock-span-problem.go
@@ -4,20 +4,22 @@
 
 package main
 
-func main(){
+import "fmt"
 
+func main() {
+	stock_prices := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(naiveApproach(stock_prices))
 }
 
-func naiveApproach (prices []int{
+func naiveApproach(prices []int) []int {
 	span := make([]int, len(prices))
-	span[0]	= 1
+	span[0] = 1
 
-	for i := range prices{
+	for i := range prices {
 		span[i] = 1
-		for j:= i-1; j >= 0 && prices[i] >= prices[j]; j--{
+		for j := i - 1; j >= 0 && prices[i] >= prices[j]; j-- {
 			span[i]++
 		}
 	}
-
+	return span
 }
-
