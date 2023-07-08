5eb376772f6201fb6bd26d4a7b788c5790429bfc
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-08 13:29:28 +0530

    refactored a lottt

---
+++ linked-list/cll.go
@@ -0,0 +1,259 @@
+package main
+
+import "fmt"
+
+// CLLNode ... newNode of circular linked list
+type CLLNode struct {
+	data int
+	next *CLLNode
+}
+
+// CLL ... A linked list with len, head
+type CLL struct {
+	size int
+	head *CLLNode
+}
+
+// NewNode ... Creating a new node
+func NewNode(data int) *CLLNode {
+	return &CLLNode{
+		data: data,
+		next: nil,
+	}
+}
+
+// NewCircularLinkedList ... Creating an empty circular linked list
+func NewCircularLinkedList() CLL {
+	return CLL{
+		size: 0,
+		head: nil,
+	}
+}
+
+func (cll *CLL) Length() int {
+	current := cll.head
+	count := 1
+	if current == nil {
+		return 0
+	}
+	current = current.next
+	for current != cll.head {
+		current = current.next
+		count++
+	}
+	return count
+}
+
+// CheckIfEmptyAndAdd ... check if doubly link list is empty
+func (cll *CLL) CheckIfEmptyAndAdd(data int) bool {
+	newNode := &CLLNode{
+		data: data,
+		next: nil,
+	}
+	// check if list is empty
+	if cll.size == 0 {
+		// insert first node in doubly linked list
+		cll.head = newNode
+		cll.head.next = newNode
+		cll.size++
+		return true
+	}
+	return false
+}
+
+// InsertBeginning ... insert in the beginning
+func (cll *CLL) InsertBeginning(data int) {
+	if !(cll.CheckIfEmptyAndAdd(data)) {
+		newNode := &CLLNode{
+			data: data,
+			next: nil,
+		}
+		current := cll.head
+
+		// insert on current
+		newNode.next = current
+
+		// traverse this list until we find it's end
+		for {
+			if current.next == cll.head {
+				break
+			}
+			current = current.next
+		}
+		current.next = newNode
+		cll.head = newNode
+		cll.size++
+	}
+}
+
+// InsertEnd ... insert in the end
+func (cll *CLL) InsertEnd(data int) {
+	if !(cll.CheckIfEmptyAndAdd(data)) {
+		newNode := &CLLNode{
+			data: data,
+			next: nil,
+		}
+		current := cll.head
+		for {
+			if current.next == cll.head {
+				break
+			}
+			current = current.next
+		}
+		current.next = newNode
+		newNode.next = cll.head
+		cll.size++
+	}
+}
+
+// Insert ... Insert in the list on a specific location
+func (cll *CLL) Insert(data int, pos int) {
+	if !(cll.CheckIfEmptyAndAdd(data)) {
+		current := cll.head
+		count := 1
+		if pos == 1 {
+			cll.InsertBeginning(data)
+			return
+		}
+		newNode := &CLLNode{
+			data: data,
+			next: nil,
+		}
+		for {
+			if current.next == nil && pos-1 > count {
+				break
+			}
+			if count == pos-1 {
+				newNode.next = current.next
+				current.next = newNode
+				cll.size++
+				break
+			}
+			current = current.next
+			count++
+		}
+	}
+}
+
+// CheckIfEmpty ... check if empty
+func (cll *CLL) CheckIfEmpty() bool {
+	if cll.size == 0 {
+		return true
+	}
+	return false
+}
+
+// DeleteBeginning ... Delete from the head of the list
+func (cll *CLL) DeleteBeginning() int {
+	//check if list if empty
+	if !(cll.CheckIfEmpty()) {
+		current := cll.head
+		deletedElem := current.data
+		if cll.size == 1 {
+			cll.head = nil
+			cll.size--
+			return deletedElem
+		}
+		prevStart := cll.head
+		cll.head = current.next
+		// traverse till end and update last node's next to updated head
+		for {
+			if current.next == prevStart {
+				break
+			}
+			current = current.next
+		}
+		current.next = cll.head
+		cll.size--
+		return deletedElem
+	}
+	return -1
+}
+
+// DeleteEnd ... Delete the last element from circular list
+func (cll *CLL) DeleteEnd() int {
+	if !(cll.CheckIfEmpty()) {
+		current := cll.head
+		deletedEle := current.data
+		if cll.size == 1 {
+			// delete from beginning
+			deletedEle = cll.DeleteBeginning()
+			return deletedEle
+		}
+		//traverse till end
+		for {
+			if current.next.next == cll.head {
+				deletedEle = current.next.data
+				break
+			}
+			current = current.next
+		}
+		// update last element's next pointer
+		current.next = cll.head
+		cll.size--
+		return deletedEle
+	}
+	return -1
+}
+
+// Delete ... delete an element from circular list
+func (cll *CLL) Delete(pos int) int {
+	if !(cll.CheckIfEmpty()) {
+		current := cll.head
+		deletedEle := current.data
+		if cll.size == 1 {
+			// delete from beginning
+			deletedEle = cll.DeleteBeginning()
+			return deletedEle
+		}
+		if cll.size == pos {
+			// delete from end
+			deletedEle = cll.DeleteEnd()
+			return deletedEle
+		}
+		// delete from middle
+		//traverse till you find position
+		count := 1
+		for {
+			if count == pos-1 {
+				deletedEle = current.next.data
+				break
+			}
+			current = current.next
+		}
+		current.next = current.next.next
+		cll.size--
+		return deletedEle
+	}
+	return -1
+}
+
+// Display ... Print Circular list
+func (cll *CLL) Display() {
+	current := cll.head
+	for i := 0; i < cll.size; i++ {
+		fmt.Print(current.data)
+		fmt.Print("-->")
+		current = current.next
+	}
+	fmt.Println()
+}
+
+/*
+func main() {
+	cll := CLL{}
+	fmt.Println("Size: ", cll.Length())
+	cll.InsertBeginning(100)
+	cll.InsertBeginning(90)
+	cll.InsertBeginning(70)
+	cll.InsertBeginning(60)
+	cll.InsertBeginning(50)
+	cll.InsertBeginning(40)
+	cll.InsertBeginning(30)
+
+	cll.InsertBeginning(19) // Update dll
+
+	cll.Display()
+	fmt.Println("Size: ", cll.Length())
+}
+*/
---
+++ linked-list/ll.go
@@ -0,0 +1,164 @@
+// https://www.geeksforgeeks.org/write-a-c-function-to-print-the-middle-of-the-linked-list/?ref=lbp
+
+package main
+
+import "fmt"
+
+type ListNode struct { // defines a ListNode in a linked list
+	data interface{} // the datum
+	next *ListNode   // pointer to the next ListNode
+}
+
+type LinkedList struct {
+	head *ListNode
+	size int
+}
+
+func (ll *LinkedList) insertAtBeginning(data interface{}) {
+	newNode := &ListNode{
+		data: data,
+	}
+	if ll.head == nil {
+		ll.head = newNode
+	} else {
+		newNode.next = ll.head
+		ll.head = newNode
+	}
+	ll.size++
+	return
+}
+
+func (ll *LinkedList) insertAtEnd(data interface{}) {
+	newNode := &ListNode{
+		data: data,
+	}
+	if ll.head == nil {
+		ll.head = newNode
+	} else {
+		current := ll.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = newNode
+	}
+	ll.size++
+	return
+}
+
+// Insert adds an item at position i
+func (ll *LinkedList) insert(position int, data interface{}) error {
+	// This condition to check whether the position given is valid or not.
+	if position < 1 || position > ll.size+1 {
+		return fmt.Errorf("insert: Index out of bounds")
+	}
+	newNode := &ListNode{data, nil}
+
+	var prev, current *ListNode
+	prev = nil
+	current = ll.head
+
+	for position > 1 {
+		prev = current
+		current = current.next
+		position = position - 1
+	}
+
+	if prev != nil {
+		prev.next = newNode
+		newNode.next = current
+	} else {
+		newNode.next = current
+		ll.head = newNode
+	}
+
+	ll.size++
+	return nil
+}
+
+func (ll *LinkedList) deleteFront() (interface{}, error) {
+	if ll.head == nil {
+		return nil, fmt.Errorf("deleteFront: List is empty")
+	}
+	data := ll.head.data
+	ll.head = ll.head.next
+	ll.size--
+	return data, nil
+}
+
+func (ll *LinkedList) deleteLast() (interface{}, error) {
+	if ll.head == nil {
+		return nil, fmt.Errorf("deleteLast: List is empty")
+	}
+	var prev *ListNode
+	current := ll.head
+	for current.next != nil {
+		prev = current
+		current = current.next
+	}
+	if prev != nil {
+		prev.next = nil
+	} else {
+		ll.head = nil
+	}
+	ll.size--
+	return current.data, nil
+}
+
+// delete removes an element at position i
+func (ll *LinkedList) delete(position int) (interface{}, error) {
+	// This condition to check whether the position given is valid or not.
+	if position < 1 || position > ll.size+1 {
+		return nil, fmt.Errorf("insert: Index out of bounds")
+	}
+	// Complete this method
+	var prev, current *ListNode
+	prev = nil
+	current = ll.head
+
+	pos := 0
+
+	if position == 1 {
+		ll.head = ll.head.next
+	} else {
+		for pos != position-1 {
+			pos = pos + 1
+			prev = current
+			current = current.next
+		}
+
+		if current != nil {
+			prev.next = current.next
+		}
+	}
+	ll.size--
+	return current.data, nil
+}
+
+func (ll *LinkedList) display() error {
+	if ll.head == nil {
+		return fmt.Errorf("display: List is empty")
+	}
+	current := ll.head
+	for current != nil {
+		fmt.Printf("%v -> ", current.data)
+		current = current.next
+	}
+	fmt.Println()
+	return nil
+}
+
+// with extra size field
+func (ll *LinkedList) length() int {
+	return ll.size
+}
+
+// length returns the linked list size
+func (ll *LinkedList) length2() int {
+	size := 0
+	current := ll.head
+	for current != nil {
+		size++
+		current = current.next
+	}
+	return size
+}
