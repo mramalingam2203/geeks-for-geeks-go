f0b5d366c134045d43b7c2296b3b1a7bf9b2d63a
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-11 11:46:30 +0530

    no progress :(

---
+++ linked-list/add-polynomials.go
@@ -0,0 +1,17 @@
+// add two polynomials using linked lists
+
+package main
+
+type Node struct {
+	pow   int
+	coeff int
+	next  *Node
+}
+
+func create_node(pow int, coeff int) {
+
+}
+
+func main() {
+
+}
