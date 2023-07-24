2452767dbb771fd1222957f3c6e3d84118d28ce0
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-24 22:44:13 +0530

    print distinct elements only

---
+++ arrays/print-distinct-elements-given-integer-array.go
@@ -0,0 +1,19 @@
+// Print All Distinct Elements of a given integer array
+
+// https://www.geeksforgeeks.org/print-distinct-elements-given-integer-array
+
+package main
+
+import(
+	"sort"
+	"fmt"
+)
+
+func main() {
+	arr := []int{1, 2, 3, 4,5, 4, 6, 7,5,5, 6,8, 9 6}
+}
+
+func printDistinctElements(array []int) {
+	fmt.Println(sort.Ints(array))
+
+}
