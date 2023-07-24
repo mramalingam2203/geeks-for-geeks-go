f3ae0f04148415a0a88379e8247c41269ac0cd2b
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-24 22:51:53 +0530

    done

--- arrays/print-distinct-elements-given-integer-array.go
+++ arrays/print-distinct-elements-given-integer-array.go
@@ -4,16 +4,24 @@
 
 package main
 
-import(
-	"sort"
+import (
 	"fmt"
+	"sort"
+	_ "sort"
 )
 
 func main() {
-	arr := []int{1, 2, 3, 4,5, 4, 6, 7,5,5, 6,8, 9 6}
+	arr := []int{1, 2, 3, 4, 5, 4, 6, 7, 5, 5, 6, 8, 9, 10, 10, 10, 11, -23, 23, 34}
+	printDistinctElements(arr)
 }
 
 func printDistinctElements(array []int) {
-	fmt.Println(sort.Ints(array))
+	sort.Ints(array)
+	fmt.Println(array[0])
+	for i := 1; i < len(array); i++ {
+		if array[i] != array[i-1] {
+			fmt.Println(array[i])
+		}
+	}
 
 }
