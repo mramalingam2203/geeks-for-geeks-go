c0085e1d934baba89ed9c707874f8f46983793ed
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-25 04:49:57 +0530

    add init code

---
+++ arrays/majority-element.go
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/majority-element
+
+package main
+
+func main() {
+
+}
+
+func findTheMajorityElement_naive(arr []int)
+func findTheMajorityElement_BST(arr []int)
+func findTheMajorityElement_mooresvoting(arr []int)
+func findTheMajorityElement_hashing(arr []int)
+func findTheMajorityElement_sorting(arr []int)
+func findTheMajorityElement_recursion(arr []int)
