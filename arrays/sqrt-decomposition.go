029bc2c096221a6d5105386eb84c22f928b6507c
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-25 09:33:53 +0530

    revisit

--- arrays/sqrt-decomposition.go
+++ arrays/sqrt-decomposition.go
@@ -6,7 +6,10 @@ This technique helps us to reduce Time Complexity by a factor of sqrt(N)
 
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	input := []int{1, 5, 2, 4, 6, 1, 3, 5, 7, 10}
@@ -14,6 +17,8 @@ func main() {
 	query(input, 3, 8)
 	query(input, 1, 6)
 	query(input, 8, 8)
+
+	preprocess(input)
 }
 
 func query(arr []int, l, r int) int {
@@ -29,9 +34,26 @@ func query(arr []int, l, r int) int {
 
 // divide blocks
 
+var arr [10000]int
+var block [100]int
+
 func preprocess(input []int) {
 	n := len(input)
-	block_size := math.Sqrt(n)
-	
+	block_size := math.Sqrt(float64(n))
+	fmt.Println(int(block_size))
 
+	blk_idx := -1
+
+	// building the decomposed array
+
+	for i := 0; i < n; i++ {
+		arr[i] = input[i]
+		if i%int(block_size) == 0 {
+			// entering next block
+			// incrementing block pointer
+			blk_idx++
+		}
+		block[blk_idx] += arr[i]
+	}
+	fmt.Println(block)
 }
