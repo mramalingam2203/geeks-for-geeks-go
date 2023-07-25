3abda366f1c9819205971355e13f299e832ab319
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-25 12:44:13 +0530

    done

--- arrays/sum-of-pairs.go
+++ arrays/sum-of-pairs.go
@@ -1,29 +1,34 @@
 // https://www.geeksforgeeks.org/sum-absolute-differences-pairs-given-array
 
-
-
 package main
 
+import (
+	"fmt"
+)
 
 func main() {
-	array := []int{1, 2, 3, 4}
+	array := []int{1, 2, 3, 4, 5, 7, 9, 11, 14}
 	sumOfPairs(array)
 
 }
 
-
-func mag(a int){
-	if a < 0{
-		return -1*a
+func mag(a int) int {
+	if a < 0 {
+		return -1 * a
 	}
 	return a
 }
 
-func sumOfPairs{arr []int) int {
-	for i := 0; i < len(arr); i++{
-		for j := i; j < len(arr); j++{
-			fmt.Println(arr[i], arr[j])
+func sumOfPairs(arr []int) int {
+	sum := 0
+
+	for i := 0; i < len(arr); i++ {
+		for j := i; j < len(arr); j++ {
+			if i != j {
+				sum += mag(arr[i] - arr[j])
+			}
 		}
 	}
-	return 0
+	fmt.Println(sum)
+	return sum
 }
