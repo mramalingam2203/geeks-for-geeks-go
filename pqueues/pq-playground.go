ee753957a8e2562d463c082f5dcd52a549490641
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-09 04:24:47 +0530

    insert new element into heap

--- pqueues/pq-playground.go
+++ pqueues/pq-playground.go
@@ -13,3 +13,15 @@ func main() {
 type MaxHeap struct {
 	heap []int
 }
+
+func (h *MaxHeap) heapifyUp(index int) {
+	for index > 0 {
+		parentIndex := (index - 1) / 2
+		if h.heap[parentIndex] >= h.heap[index] {
+			break
+		}
+		h.heap[parentIndex], h.heap[index] = h.heap[index], h.heap[parentIndex]
+		index = parentIndex
+	}
+
+}
